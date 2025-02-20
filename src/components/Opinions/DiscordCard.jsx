c1ee7b81290df3e8685bd0ba0945c1a633c1c91e
Eaulmesse <theo.guerin01120@gmail.com>
2025-02-20 15:27:12 +0100

    edit: solving responsive error

--- src/components/Opinions/DiscordCard.jsx
+++ src/components/Opinions/DiscordCard.jsx
@@ -6,23 +6,19 @@ import discord from '../../assets/images/discord-logo.png'
 const DiscordCard = ({ thumbnail, title, link, text, width }) => {
     return (
         <motion.div 
-            
             className={`bg-[#7289da] rounded-2xl readex-pro text-white py-5 ${width} h-full cursor-pointer relative shadow-custom`}
             onClick={() => window.open('https://discord.gg/Wdhy2EA3', '_blank')}
         >
-            <div className='w-5/6 ml-5 flex space-x-2'>
-                <img src={discord} alt="Discord" className='w-12 h-12 object-cover rounded-lg shadow-lg'/>
+            <div className='md:flex space-x-2 w-5/6 m-auto md:ml-5 text-center md:text-left'>
+                <img src={discord} alt="Discord" className='w-12 h-12 object-cover rounded-lg shadow-lg m-auto md:m-0'/>
                 <div>
-                    <p className='text-lg font-semibold'>{title}</p>
-                    <p>{link}</p>
+                    <p className='w-5/6 m-auto md:m-0 lg:text-lg font-semibold'>{title}</p>
+                    <p className='w-5/6 text-xs'>{link}</p>
                 </div>
             </div>
 
-            <div className='flex h-fit ml-5 flex self-end  items-center'>
-                <p className='text-lg w-full h-fit'>
-                    {text} 
-                    
-                </p>
+            <div className='flex mt-5 ml-5 flex self-end items-center'>
+                <p className='text-lg w-full '>{text}</p>
                 <ChevronRight  size={40} color='white'/>
             </div>
             
