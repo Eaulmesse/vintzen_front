2095dad861e9086667dc7feb7918fa5e758c12ae
Eaulmesse <theo.guerin01120@gmail.com>
2025-02-20 16:49:14 +0100

    add: pricing div

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -12,9 +12,23 @@ const Pricing = () => {
 
 
     return (
-        <div>
-            <h1>Notre offre actuelle</h1>
-            <p>Voici nos plans tarifaires.</p>
+        <div className='w-custom readex-pro p-10 m-auto bg-custom-green rounded-xl shadow-custom flex flex-col lg:flex-row gap-2'>
+            <div className='w-full lg:w-1/2 h-full lg:m-auto flex flex-col justify-between'>   
+                <h2 className='text-5xl font-bold text-white'>Notre offre <span className='text-custom-purple'>actuelle</span></h2>
+
+                <p className='mt-5 w-full lg:w-4/5 text-white'>
+                    L'outil étant actuellement en <span className='text-custom-purple'>bêta</span>, il vous est <span className='text-custom-purple'>accessible gratuitement ! </span>
+                    Si vous souhaitez nous aider, faites nous un maximum de <span className='text-custom-purple'> retour d'experience </span> pour <span className='text-custom-purple'> améliorer notre offre </span> et nos <span className='text-custom-purple'> fonctionalitées. </span>
+                </p>
+
+                <p className='text-white font-bold mt-5 w-full lg:w-4/5'> <span className='text-custom-purple'> Plusieurs offres </span> seront disponibles selon vos besoins au lancement officiel de la solution.</p>
+            </div>
+            
+            <div className='w-full lg:w-1/2 m-auto lg:m-0 mt-10'>
+                <PricingCard title={"Bêta Tester"} price={"Gratuit"} features={freeFeatures} link={"#"} />
+            </div>
+            
+            
         </div>
     );
 };
--- src/components/Pricing/PricingCard.jsx
+++ src/components/Pricing/PricingCard.jsx
@@ -4,47 +4,30 @@ import { motion } from "motion/react";
 
 const PricingCard = ({ title, price, features, link }) => {
     return (
-        <div className='bg-white w-4/5 lg:w-[30%] m-auto mt-10 py-5 rounded-lg shadow-lg'>
+            
+        
+        <div className='bg-white m-auto py-5 rounded-xl w-[80%] shadow-xl'>
             {/* Title */}
-            <motion.p 
-                initial={{ opacity: 0, scale: 0.75 }} 
-                whileInView={{ opacity: 1, scale: 1 }} 
-                transition={{ delay: 0.25, duration: 1 }} 
-                viewport={{ once: true }} 
-                className='m-auto bg-blue-100 text-blue-600 font-semibold w-fit py-1 px-4 rounded-full'
-            >
+            <p  className='m-auto bg-custom-green bg-opacity-20 text-custom-green font-semibold w-fit py-1 px-4 rounded-full' >
                 {title}
-            </motion.p>
+            </p>
 
             {/* Price */}
-            <motion.div  
-                initial={{ opacity: 0, scale: 0.75 }} 
-                whileInView={{ opacity: 1, scale: 1 }} 
-                transition={{ delay: 0.40, duration: 1 }} 
-                viewport={{ once: true }} 
-                className='w-fit m-auto text-center mt-5'
-            >
+            <div className='w-fit m-auto text-center mt-5'>
                 <p className='text-3xl font-semibold'>{price}</p>
-                <p className='text-gray-600'>Par mois</p>
-            </motion.div>
+            </div>
 
             {/* Features */}
-            <motion.div  
-                initial={{ opacity: 0, scale: 0.75 }} 
-                whileInView={{ opacity: 1, scale: 1 }} 
-                transition={{ delay: 0.50, duration: 1 }} 
-                viewport={{ once: true }} 
-                className='w-4/5 m-auto mt-5 text-left'
-            >
+            <div className='w-4/5 m-auto mt-5 text-left'>
                 <ul className='space-y-3'>
                     {features.map((feature, index) => (
-                        <li key={index} className='flex items-start space-x-3'>
-                            <span className='inline-block w-4 h-4 bg-blue-600 rounded-full mt-1'></span>
-                            <p className='text-gray-600'>{feature}</p>
+                        <li key={index} className='flex items-center space-x-3 bg-custom-green bg-opacity-20 rounded-lg p-2 shadow'>
+                            <span className='min-w-2 h-8 bg-custom-green rounded-full mt-1'></span>
+                            <p className='text-gray-600 font-semibold'>{feature}</p>
                         </li>
                     ))}
                 </ul>
-            </motion.div>
+            </div>
 
             {/* Call to Action Button */}
             <motion.div  
@@ -54,7 +37,7 @@ const PricingCard = ({ title, price, features, link }) => {
                 viewport={{ once: true }} 
                 className='mt-5 w-4/5 m-auto text-center'
             >
-                <BlueButton href={link} text={"Choisir cet abonnement"} />
+                
             </motion.div>
         </div>
     );
