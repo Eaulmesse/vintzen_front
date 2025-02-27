cb6105864964445f16b7e0e576f5b283270d7f48
Eaulmesse <theo.guerin01120@gmail.com>
2025-02-27 15:18:44 +0100

    edit: remove shadow-custom from Faq

--- src/components/Faq/FaqQuestion.jsx
+++ src/components/Faq/FaqQuestion.jsx
@@ -6,7 +6,7 @@ const FaqQuestion = ({ question, answer }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className='mx-auto my-5 rounded-lg w-custom-indiv bg-custom-purple readex-pro p-5 border-custom-purple shadow-custom text-white' onClick={() => setIsOpen(!isOpen)}>
+        <div className='mx-auto my-5 rounded-lg w-custom-indiv bg-custom-purple readex-pro p-5 border-custom-purple  text-white' onClick={() => setIsOpen(!isOpen)}>
             <div className='flex justify-between w-full'>
                 <p className='text-xl'>{question}</p>
                 {isOpen ? (
